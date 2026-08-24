54aa3574536e1028283e3f6b83d7d78a06f00a88
unknown <saini.sunnnyy@gmail.com>
2026-08-25 00:01:15 +0530

    ghewar_khalo_guys

--- Parent_Module/main.tf
+++ Parent_Module/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg_kewar" {
   name     = "rg-ghewar"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "rg_Malaiya" {
+  name     = "rg-ghewariya"
+  location = "eastus"
+}
