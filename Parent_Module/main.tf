c3f05c0c46da6780dd95d3b154f8d62cc915053b
unknown <saini.sunnnyy@gmail.com>
2026-08-25 00:12:23 +0530

    self_hosted

--- Parent_Module/main.tf
+++ Parent_Module/main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "rg_Malaiya" {
   name     = "rg-ghewariya"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "rg_Kalua" {
+  name     = "rg-rasmalai"
+  location = "eastus"
+}
